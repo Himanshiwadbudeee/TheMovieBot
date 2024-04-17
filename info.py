71ecca5799c288ebf5638f56bcac903bafdacbad
Himanshiwadbudeee <166972430+Himanshiwadbudeee@users.noreply.github.com>
2024-04-17 13:55:28 +0530

    PAID EDIT BY @nawab_me

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '24363060'))
-API_HASH = environ.get('API_HASH', '2403084c19a00e8f07874b15ba01557b')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6102671698:AAGQJcQ-ADVvuoIPRDgxJnawAJtbnpzbYMA")
+API_ID = int(environ.get('API_ID', '25209369'))
+API_HASH = environ.get('API_HASH', '2a1bd33d305f310578f27453e17e45c6')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6313971531:AAE11XHgIUYe1fBFHnKqHLFscJSp2JVyM-0")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 400))
@@ -27,7 +27,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6029347235').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '841012006').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001717761625').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -58,13 +58,13 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/nawab_me1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/nawabbotupdate')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/nawabbotupdate/15')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/LatestmoviesHQz')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TheUpdateChanne')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/LatestmoviesHQz')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ🥀')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001849404517'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/nawabbotupdate')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002061248145'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/TheUpdateChanne')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
