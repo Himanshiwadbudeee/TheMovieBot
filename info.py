ff3b5b31ccb7e75cc2cbb472d446c6a1a1b6f882
Himanshiwadbudeee <166972430+Himanshiwadbudeee@users.noreply.github.com>
2024-06-04 08:37:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,10 +49,10 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY',False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'atglinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '86771f081e5cd907098e9c73da72d1d7526498d9')
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'atglinks.com')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '86771f081e5cd907098e9c73da72d1d7526498d9')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'zipshort.net')
+SHORTLINK_API = environ.get('SHORTLINK_API', '11a3ab730087bd78a4a9365d39c6a1a3dcba4cf9')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'zipshort.net')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '11a3ab730087bd78a4a9365d39c6a1a3dcba4cf9')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True ))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
